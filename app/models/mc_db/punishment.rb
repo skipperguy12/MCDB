e4f25c2d6023b20076dabd3c1804887c5ea2b8ab
Austin Mayes <bookofnathan@gmail.com>
2014-07-11 22:58:50 -0500

    DateTimes mot Times

--- app/models/mc_db/punishment.rb
+++ app/models/mc_db/punishment.rb
@@ -1,14 +1,28 @@
 module McDb
   class Punishment
     include Mongoid::Document
+    include Mongoid::Timestamps
     field :reason, type: String
-    field :created, type: Time
-    field :expires, type: Time
+    field :created, type: DateTime
+    field :expires, type: DateTime
     field :type, type: String
     field :punisher, type: String
     field :punished, type: String
     field :appealable, type: Mongoid::Boolean
     field :cluster, type: String
     field :active, type: Mongoid::Boolean
+    field :server, type: String
+
+    def reason_color
+        if self.type == "warn"
+            "green"
+        elsif self.type == "kick"
+            "orange"
+        elsif self.type == "ban"
+            "red"
+        else
+            "black"
+        end
+    end
   end
 end
