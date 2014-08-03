0de08ec07698d89fdfd16662c7bbaa5a2e3b0005
Austin Mayes <bookofnathan@gmail.com>
2014-08-03 15:15:22 -0500

    Fix some fields that slipped by

--- app/models/mc_db/session.rb
+++ app/models/mc_db/session.rb
@@ -2,11 +2,11 @@ module McDb
   class Session
     include Mongoid::Document
     store_in collection: "dndb_sessions", database: "dndb_sessions"
-    field :server, type: ObjectId
+    field :server, type: BSON::ObjectId
     field :start, type: DateTime
     field :end, type: DateTime
     field :length, type: Integer
-    field :user, type: UUID
+    field :user, type: BSON::ObjectId
     field :ended_correctly, type: Mongoid::Boolean
     field :ended_with_punish, type: Mongoid::Boolean
     field :ip, type: String
